e886b0b3c55f57582f54db1dde5e60f941388914
Boakai <boakaidkamara@gmail.com>
2022-06-26 06:24:10 +0000

    fixed bug in view

--- I4G008413HKW/blog/views.py
+++ I4G008413HKW/blog/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+from .models import Post
 
 
 # Create your views here.
-class PostListView (generic.PostListView):
+class PostListView (generic.ListView):
     model = Post
 
 
-class PostCreateView (generic.PostCreateView):
+class PostCreateView (generic.CreateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
 
 
 class PostDetailView (generic.DetailView):
@@ -19,11 +21,11 @@ class PostDetailView (generic.DetailView):
 
 class PostUpdateView(generic.UpdateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
 
 
 class PostDeleteView(generic.DeleteView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
