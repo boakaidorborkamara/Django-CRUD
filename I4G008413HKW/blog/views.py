661975168b57379f94472085ea51fe3729447c0e
Boakai <boakaidkamara@gmail.com>
2022-06-26 05:56:52 +0000

    created view from django generic view

--- I4G008413HKW/blog/views.py
+++ I4G008413HKW/blog/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
-# def list_view(request):
+class PostListView (generic.PostListView):
+    model = Post
+
+
+class PostCreateView (generic.PostCreateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+
+class PostDetailView (generic.DetailView):
+    model = Post
+
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
